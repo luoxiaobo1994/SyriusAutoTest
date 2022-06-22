001cf3bd82fe06b22adf8857ff4e1040231a3386
luoxiaobo <luoxiaobo@lxb.com>
2022-06-22 19:32:16 +0800

    根据ID查找订单。

--- Syrius_API/RouteMaker/mainuse.py
+++ Syrius_API/RouteMaker/mainuse.py
@@ -5,7 +5,7 @@
 import os
 
 # 传入你地图文件的地址.地图ID/map_01/common 到这一级目录.
-file = r'E:\工作\地图\有shel_location文件的地图\spare_map_1679c01\spare_map_1679c01\map_01\common'
+file = r'E:\工作\地图\临时地图\eb20_9cb7_03\eb20_9cb7_03\map_01\common'
 # 不要直接运行脚本,在pycharm里打开是看不到坐标的,只能看到图.使用命令行运行这个py文件
 # ...\Syrius_API\RouteMaker>  python .\mainuse.py
 os.system(f"Python route_ui.py {file}")
