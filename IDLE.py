630f5b979434bc66732d2d9802323ea64c0a92d1
luoxiaobo <438119195@qq.com>
2022-06-23 20:35:18 +0800

    Changes

--- IDLE.py
+++ IDLE.py
@@ -2,18 +2,15 @@
 # Author:Luoxiaobo
 # Time: 2021/7/6 23:09
 
-import random
 import os
-from utils.file_reader import YamlReader
-import yaml
-from Syrius_API.flagship.res_notify import send_order
-from utils.log import logger
-from time import sleep
-import itertools
-from base.common import *
 
-def x():
-    return 1,[1]
-a = x()
-if a:
-    print(a[1])
+file = "D:\SoftData\Feishu\piplist.txt"
+with open(file) as f:
+    con = f.readlines()
+for i in con:
+    ku = i.split()[0]
+    try:
+        os.system(f"pip install {ku}")
+    except Exception as e:
+        print(e)
+
