ae60a7f7696fc373c58cab8aaeb5f680e2481201
luoxiaobo <438119195@qq.com>
2022-08-01 19:19:06 +0800

    .

--- config/readyml_demo.py
+++ config/readyml_demo.py
@@ -7,19 +7,19 @@ from base.common import *
 from utils.file_reader import YamlReader
 
 file = "yaml_demo.yaml"
-data = YamlReader(file).data
+data = read_yaml(file)
 print(f"全部数据：{data}")
-print("-*" * 20)
-for i in data:
-    x = str(type(data[i]))
-    print(f"type:{x:<23}{i}: {data[i]}")
+# print("-*" * 20)
+# for i in data:
+#     x = str(type(data[i]))
+#     print(f"type:{x:<23}{i}: {data[i]}")
 
-if data['yes11']:
-    print('yes11')
-if data['no22']:  # 正确读取为False的.
-    print('no22')
-
-# update_yaml('site_info.yaml', {'api_order': False})
-data = read_yaml('site_info.yaml')
-print(data[data['SpeedPicker_cn2']])
-print(get_filename())
+# if data['yes11']:
+#     print('yes11')
+# if data['no22']:  # 正确读取为False的.
+#     print('no22')
+#
+# # update_yaml('site_info.yaml', {'api_order': False})
+# data = read_yaml('site_info.yaml')
+# print(data[data['SpeedPicker_cn2']])
+# print(get_filename())
