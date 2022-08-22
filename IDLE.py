08e1f86794819b1305fb623ab0d30c303eac951a
luoxiaobo <438119195@qq.com>
2022-08-22 15:08:17 +0800

    .

--- IDLE.py
+++ IDLE.py
@@ -2,3 +2,8 @@
 # Author:Luoxiaobo
 # Time: 2021/7/6 23:09
 
+from utils.log2 import Logger
+
+file = __file__.split('\\')[-1].replace('.py', '.txt')
+logger = Logger(file=file).get_logger()
+logger.debug(f"当前日志记录文件为：{file}")
--- Syrius_UI/SpeedPicker_cn1.py
+++ Syrius_UI/SpeedPicker_cn1.py
@@ -16,7 +16,7 @@ from utils.log2 import Logger
 
 file = __file__.split('\\')[-1].replace('.py', '.txt')
 logger = Logger(file=file).get_logger()
-logger.debug(f"当前日志记录文件为：{file}.txt")
+logger.debug(f"当前日志记录文件为：{file}")
 
 
 class SpeedPicker:
