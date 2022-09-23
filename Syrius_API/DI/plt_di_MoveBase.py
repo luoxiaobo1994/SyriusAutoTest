d1adbbe83730108ace522c211164f26779b0657f
luoxiaobo <luoxiaobo@lxb.com>
2022-09-23 17:07:18 +0800

    。

---
+++ Syrius_API/DI/plt_di_MoveBase.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+# Author: luoxiaobo
+# TIME: 2022/9/9 15:46
+# Desc: 画出近5个版本的DI值
+
+from matplotlib import pyplot as plt
+
+from base.common import read_yaml
+
+plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置字体
+plt.rcParams["axes.unicode_minus"] = False  # 该语句解决图像中的“-”负号的乱码问题
+
+data = read_yaml('DI_MoveBase.yaml')
+
+
+def plt_DI():
+    x = list(data.keys())[-5:]
+    y = []
+    bug = list(data.values())[-5:]  # 先获取到最新5个版本的BUG分布情况
+    for i in bug:
+        y.append(i['P0'] * 10 + i['P1'] * 3 + i['P2'] * 1 + i['P3'] * 0.1)
+    print(y)
+    plt.plot(x, y, marker='o', label='DI值')
+    plt.xlabel("MoveBase版本")
+    plt.ylabel("DI值")
+    plt.title("近5个MoveBase版本DI值变化曲线")
+    for a, b in zip(x, y):
+        plt.text(a, b, b, ha='center', va='bottom', fontsize=15)
+    plt.legend()
+    plt.show()
+
+
+# print(data.keys())
+plt_DI()
--- Syrius_UI/screenshoot.py
+++ Syrius_UI/screenshoot.py
@@ -6,6 +6,7 @@
 from base.common import *
 
 if __name__ == '__main__':
-    app_screenshot()  # 仅能指定文件名称,文件路径已经写死.
+    # app_screenshot()  # 仅能指定文件名称,文件路径已经写死.
     # app_screenshot('10.2.10.229:5555')  # 仅能指定文件名称,文件路径已经写死.
+    app_screenshot('10.2.11.51:5555')  # 仅能指定文件名称,文件路径已经写死.
     # app_screenshot('10.2.11.107:5555')  # 仅能指定文件名称,文件路径已经写死.
