306e9b7b03de5f3877fab599eb04130a23ed8e36
luoxiaobo <luoxiaobo@lxb.com>
2022-11-09 22:08:32 +0800

    。

--- IDLE.py
+++ IDLE.py
@@ -2,25 +2,50 @@
 # Author:Luoxiaobo
 # Time: 2021/7/6 23:09
 
-from threading import Thread
-from time import sleep
+import json
+
+import requests
 
 
-class a():
+def get_data():
+    data = {
+        "msg_type": "post",
+        "content": {
+            "post": {
+                "zh_cn": {
+                    "title": "项目更新通知",
+                    "content": [
+                        [{
+                            "tag": "text",
+                            "text": "项目有更新: "
+                        },
+                            {
+                                "tag": "a",
+                                "text": "请查看",
+                                "href": "https://www.baidu.com/"
+                            },
+                            {
+                                "tag": "at",
+                                "user_id": "123456"
+                            }
+                        ]
+                    ]
+                }
+            }
+        }
+    }
+    return json.dumps(data, ensure_ascii=True).encode("utf-8")
 
-    def a_a(self):
-        while True:
-            print('.')
-            sleep(1)
 
-    def a_b(self):
-        while True:
-            print('..')
-            sleep(0.5)
+def req(data):
+    url = "https://open.feishu.cn/open-apis/bot/v2/hook/f27cb489-5c80-45b5-afc5-0446fe289cce"
+    header = {
+        "Content-type": "application/json",
+        "charset": "utf-8"
+    }
+    res = requests.post(url, data=data, headers=header)
+    print(res.status_code)
 
 
-a1 = a()
-tha = Thread(target=a1.a_a)
-thb = Thread(target=a1.a_b)
-tha.start()
-thb.start()
+if __name__ == '__main__':
+    req(get_data())
--- Syrius_API/other/feishu_robot.py
+++ Syrius_API/other/feishu_robot.py
@@ -3,10 +3,76 @@
 # TIME: 2022/11/9 19:26
 # Desc: 飞书机器人，提醒打卡。
 
-import hashlib
 import base64
+import hashlib
 import hmac
+
 import requests
+
+
+class FeiShuRobot():
+
+    def __init__(self, app_id='', app_secret='', chat_name=''):
+        self.app_id = 'cli_a3e124a3d0389013'
+        self.app_secret = 'Ak3KrBUZ1NFXSGFijYCInd1ivu76FIIx'
+        self.chat_name = chat_name
+        self.access_token = self.get_access_token()
+        self.url = "https://open.feishu.cn/open-apis/bot/v2/hook/f27cb489-5c80-45b5-afc5-0446fe289cce"
+        self.headers = {
+            "Authorization": "Bearer {}".format(self.access_token),
+            "Content-Type": "application/json"
+        }
+
+    def get_access_token(self):
+        data = {
+            "app_id": self.app_id,
+            "app_secret": self.app_secret
+        }
+        try:
+            res = requests.post("https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/", json=data)
+            if res.status_code == 200:
+                res_json = res.json()
+                access_token = res_json.get("tenant_access_token")
+                print(access_token)
+                return access_token
+        except Exception as e:
+            return {"error": e}
+
+    # 获取群列表
+    def get_chat_list(self):
+        params = {
+            "page_size": 100,
+            "page_token": ""
+        }
+        try:
+            res = requests.get("https://open.feishu.cn/open-apis/chat/v4/list", params=params, headers=self.headers)
+            if res.status_code == 200:
+                res_json = res.json()
+                data = res_json.get("data")
+                groups = data.get("groups")
+                print(f"群组：{groups}")
+                for i in groups:
+                    if i.get("name") == self.chat_name:
+                        return i
+        except Exception as e:
+            return {"error": e}
+
+    def send_msg(self, text):
+        res = self.get_chat_list()
+        chat_id = res.get("chat_id")
+
+        data = {
+            "chat_id": chat_id,
+            "msg_type": "text",
+            "content": {
+                "text": text
+            }
+        }
+        try:
+            res = requests.post("https://open.feishu.cn/open-apis/message/v4/send/", headers=self.headers, json=data)
+            return res.json()
+        except Exception as e:
+            return {"error": e}
 
 
 def gen_sign(timestamp, secret):
@@ -20,11 +86,7 @@ def gen_sign(timestamp, secret):
     return sign
 
 
-def send_msg(msg=''):
-    web_hook = "https://open.feishu.cn/open-apis/bot/v2/hook/f27cb489-5c80-45b5-afc5-0446fe289cce"
-    data_1 = {"msg_type":"text","content":{"text":"测试机器人"}}
-    requests.post(url=web_hook,data=data_1)
-    # print()
-
 if __name__ == '__main__':
-    send_msg()
+    fei = FeiShuRobot()
+    fei.get_chat_list()
+    # res = fei.send_msg("I am coming")
