6fff855efff500cf86e34a3f305b5effa14152f2
redmibook16 <438119195@qq.com>
2022-12-08 16:50:40 +0800

    .

--- Syrius_UI/1check_info.py
+++ Syrius_UI/1check_info.py
@@ -4,6 +4,7 @@
 # Desc: 测试前检查机器人测试环境信息。
 
 import datetime
+import os
 import re
 import time
 
@@ -20,9 +21,15 @@ robot = {
 }
 
 
-def pp(msg, level='DEBUG'):
-    # 充当log函数
-    print(f"{datetime.datetime.now()} [{level}] : {msg}")
+def pp(msg, level='DEBUG', color=''):
+    if not color:
+        # 充当log函数
+        print(f"{datetime.datetime.now()} [{level}] : {msg}")
+    else:
+        if color in ['g', 'green', 'GREEN', 'Green']:
+            print(f"\033[1;36m{datetime.datetime.now()} [{level}] : {msg}\033[0m")
+        elif color in ['r', 'red', 'RED', 'Red']:
+            print(f"\033[1;31m{datetime.datetime.now()} [{level}] : {msg}\033[0m")
 
 
 def sshLogin(ip, port, username='factory', passwd='factory'):
@@ -35,7 +42,7 @@ def sshLogin(ip, port, username='factory', passwd='factory'):
         pp(msg=f"开始连接：[{ip}], 端口为：[{str(port)}], 账号为：[{username}], 密码为：[{passwd}]。")
         pp(f"连接[{ip}]成功。")
     except TimeoutError:
-        pp(msg=f"连接[]失败，失败原因：超时。", level='WARNING')
+        pp(msg=f"连接[]失败，失败原因：超时。", level='WARNING', color='r')
 
 
 def exe_cmd(cmd='ls', isreturn=True, printres=False, timeout=3, username='factory', passwd='factory'):
@@ -51,10 +58,10 @@ def exe_cmd(cmd='ls', isreturn=True, printres=False, timeout=3, username='factor
         time.sleep(0.5)
         stdin.flush()
     if 'Permission denied' in result:
-        pp(f'执行命令：{cmd}的权限不够，请检查。', 'WARNING')
+        pp(f'执行命令：{cmd}的权限不够，请检查。', 'WARNING', color='r')
     if isreturn:
         if printres:
-            pp(f"命令的返回结果：{result}", 'DEBUG')
+            pp(f"命令的返回结果：{result}", 'DEBUG', color='r')
         return result
 
 
@@ -63,28 +70,31 @@ def sshClose():
     global ssh
     ssh.close()
     pp(f"操作完毕，关闭ssh连接。")
-    pp('*-' * 40)
+    pp('*-' * 20)
 
 
 # 以下是具体业务函数。-----------------------------------------------------------------
 def basic_info():
     # 检查MoveBase版本
     MoveBase = exe_cmd('cat /opt/cosmos/etc/ota/version')
-    pp(f'MoveBase版本：{MoveBase}')
+    pp(f'MoveBase版本：{MoveBase}', color='g')
     # 检查L4T信息
     L4T = exe_cmd("grep -E 'build date:(.*?)$' /etc/version.yaml")
-    pp(f"L4T信息：{L4T}")
+    pp(f"L4T信息：{L4T}", color='g')
     # 检查SN
     SN = exe_cmd('cat /sys/robotInfo/RobotSN')
     SN = SN.split()[0]  # 直接替换换行符有点问题。
-    pp(f"机器人的SN：{SN}")
+    pp(f"机器人的SN：{SN}", color='g')
     # 检查ID
     ID = exe_cmd('dbus-send --system --print-reply=literal --type=method_call --dest=com.'
                  'syriusrobotics.secbot /buzzard/secbot com.syriusrobotics.secbot.ISecBot.getDroidId')
-    pp(f"机器人的ID：{ID.split()[0]}")  # 去掉  int32 0
+    pp(f"机器人的ID：{ID.split()[0]}", color='g')  # 去掉  int32 0
     # 检查Java进程数量
     java_process = exe_cmd('ps -aux | grep java | wc -l')
-    pp(f"Java进程数量：{java_process}，{'正常。' if java_process >= '10' else '数量异常，请检查！'}")
+    if java_process >= '10':
+        pp(f"Java进程数量：{java_process}，{'正常。'}", color='g')
+    else:
+        pp(f"Java进程数量：{java_process}，{'正常。'}", color='r')
 
 
 def calibartion():
@@ -92,18 +102,18 @@ def calibartion():
     res = exe_cmd("grep -E 'Sensors:' /opt/cosmos/etc/calib/calibration_result/robot_sensors.yaml")
     res2 = exe_cmd("ls -lh /opt/cosmos/etc/calib/calibration_result/robot_sensors.yaml").split()[4]
     if res and res2:
-        pp(f"机器人的标定文件正常。")
+        pp(f"机器人的标定文件正常。标定文件大小：{res2}", color='g')
     else:
-        pp(f"机器人的标定文件检查异常，文件不存在或为空。")
+        pp(f"机器人的标定文件检查异常，文件不存在或为空。", color='r')
 
 
 def check_time(repair=True):
     # 检查机器人时间
     res = exe_cmd("date +'%Y-%m-%d %H:%M:%S'")
-    pp(f"机器人当前时间：{res}")
+    pp(f"机器人当前时间：{res}", color='g')
     now = str(datetime.datetime.now())
     if res[:10] != now[:10]:
-        pp(f"机器人当前时间与实际UTC时间差距较大，请检查！", "WARNING")
+        pp(f"机器人当前时间与实际UTC时间差距较大，请检查！", "WARNING", color='r')
         now1 = list(time.localtime())
         date = ''.join([str(i) for i in now1[:3]])
         h = now1[3] - 8  # 基本是上班时间才会去检查。所以减8没问题。半夜做，就有BUG。
@@ -125,7 +135,7 @@ def diskUsage():
     percent = re.findall('\d+%', res)[0]
     threshold = '85%'
     if percent > threshold:
-        pp(f"机器人的磁盘占用大于{threshold}，执行:1.日志清除命令。2.删除home目录下的更新包")
+        pp(f"机器人的磁盘占用大于{threshold}，执行:1.日志清除命令。2.删除home目录下的更新包", color='r')
         exe_cmd('sudo journalctl --vacuum-size=1K')
         exe_cmd('rm -rf ./update_*')
 
@@ -137,23 +147,21 @@ def clearOTA():
     pp("清除OTA缓存。")
 
 
-def env(count=3):
+def env(ip,port='22',count=3):
     res1 = exe_cmd('cat /opt/cosmos/bin/ota/checker/application.yml')
     res2 = exe_cmd('cat /opt/cosmos/bin/iot-gateway/application.yml')
     res3 = exe_cmd('cat /opt/cosmos/bin/secbot/application.yml')
     if 'env: test' in res1 and res1 == res2 == res3:
-        pp(f"机器人的环境为：{res1}")
+        pp(f"机器人的环境为：{res1}", color='g')
     else:
-        pp(f"机器人的环境文件缺失，手动添加配置文件。")
+        pp(f"机器人的环境文件缺失，使用echo创建测试环境配置文件。", color='r')
         try:
-            cmd1 = 'sudo echo env: test > /opt/cosmos/bin/ota/checker/application.yml'
-            cmd2 = 'sudo echo env: test > /opt/cosmos/bin/iot-gateway/application.yml'
-            cmd3 = 'sudo echo env: test > /opt/cosmos/bin/secbot/application.yml'
-            exe_cmd(cmd1, printres=True)
-            exe_cmd(cmd2)
-            exe_cmd(cmd3)
+            exe_cmd("echo 'env: test' > /opt/cosmos/bin/secbot/application.yml")
+            exe_cmd("echo 'env: test' > /opt/cosmos/bin/iot-gateway/application.yml")
+            exe_cmd("echo 'env: test' > /opt/cosmos/bin/ota/checker/application.yml")
+            pp(f"环境配置文件创建完成。", color='g')
         except TimeoutError:
-            pp(f"写环境文件命令超时，请检查。")
+            pp(f"环境配置文件创建失败，请检查。", color='r')
 
 
 def model():
@@ -171,41 +179,40 @@ def model():
     }
     try:
         res = exe_cmd('cat /sys/robotInfo/Model')[:9]
-        pp(f"机器人Model是：{res}，对应机型：{model[res] if model.get(res) else '没有对应机型，请检查。'}")
+        pp(f"机器人Model是：{res}，对应机型：{model[res] if model.get(res) else '没有对应机型，请检查。'}",color='g')
     except TypeError:
-        pp(f"机器人Model查询结果返回异常，请检查。")
+        pp(f"机器人Model查询结果返回异常，请检查。",color='r')
 
 
 def debug():
     exe_cmd('sudo ls')
 
 
-def main(ip='10.2.16.200',port=22):
-    sshLogin(ip, port, 'developer', 'developer')
+def main(ip='10.2.16.200', port=22):
+    sshLogin(ip=ip, port=port, username='developer', passwd='developer')
     basic_info()
     calibartion()
-    # check_time()
-    # diskUsage()
-    # model()
-    # env()
+    check_time()
+    diskUsage()
+    model()
+    env(ip=ip)
     # debug()
     sshClose()
 
 
 if __name__ == '__main__':
     robot = {
-        '雷龙·苏亚雷斯':'10.2.9.181',
-        '雷龙·内马尔':'10.2.8.255',
+        '雷龙·苏亚雷斯': '10.2.9.181',
+        '雷龙·内马尔': '10.2.8.255',
         '雷龙·齐达内': '10.2.8.65',
         '雷龙·C罗': '10.2.8.118',
-        '梁龙·鸣人':'10.2.8.103',
-        '梁龙·索隆':'10.2.8.211',
-        '梁龙·佐助':'10.2.8.77',
+        '梁龙·鸣人': '10.2.8.103',
+        '梁龙·索隆': '10.2.8.211',
+        '梁龙·佐助': '10.2.8.77',
     }
-    main('雷龙·苏亚雷斯')
-    # main('雷龙·内马尔')
-    # main('雷龙·齐达内')
-    # main('雷龙·C罗')
-    # main('梁龙·鸣人')
-    # main('梁龙·佐助')
-    # main('梁龙·索隆')
+    main(robot['雷龙·苏亚雷斯'])
+    # main(robot['雷龙·齐达内'])
+    # main(robot['雷龙·C罗'])
+    # main(robot['梁龙·鸣人'])
+    # main(robot['梁龙·索隆'])
+    # main(robot['梁龙·佐助'])
--- Syrius_UI/1screenshoot.py
+++ Syrius_UI/1screenshoot.py
@@ -5,6 +5,7 @@
 
 from base.common import *
 
+
 if __name__ == '__main__':
     app_screenshot()  # 仅能指定文件名称,文件路径已经写死.
     # app_screenshot('10.2.10.229:5555')  # 仅能指定文件名称,文件路径已经写死.
