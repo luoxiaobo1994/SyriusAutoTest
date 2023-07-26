47ae96e6423ea4e9140fe4876aa7148c5d1b7e5f
luoxiaobo <luoxiaobo@syriusrobotics.com>
2023-07-26 10:24:34 +0800

    更新ignore。

--- IDLE.py
+++ IDLE.py
@@ -3,17 +3,13 @@
 # 2023/4/24 19:39
 # Describe: 本地调试脚本
 
+from base.common import *
+from collections.abc import Iterable
 
-def tt(func):
-    def gf(*args):
-        print("in gf")
-        func(*args)
+class Student:
 
-    return gf
+    def __iter__(self):
+        pass
 
-
-@tt
-def foo(x):
-    print(f"in foo",x)
-
-foo(7)
+dp(isinstance(Student(),Iterable))
+dp(hasattr(Student(),'__getitem__'))
